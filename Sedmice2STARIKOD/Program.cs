c152a95bf8f700f6bdf0158d81d88b2c07846341
Miljan <miljan.dujovicmd@gmail.com>
2020-05-19 01:22:58 +0200

    isContains

--- Sedmice2STARIKOD/Program.cs
+++ Sedmice2STARIKOD/Program.cs
@@ -17,6 +17,18 @@ namespace Sevens
             }
             
         }
+        public bool isContains(Card card)
+        {
+            foreach (Card c in cards)
+            {
+                if ((c.name == card.name) & (c.symbol == card.symbol))
+                {
+                    return true;
+                }
+ 
+            }
+            return false;
+        }
         public void create4Cards(string name)
         {
             this.cards.Add(new Card(name, 3));
@@ -62,8 +74,12 @@ namespace Sevens
         {
             Deck deck = new Deck();
             //deck.addCard(new Card("7", 4));
-
-
+            //deck.addCard(new Card("8", 5));
+            
+            //if (deck.isContains(new Card("7", 4)))
+            //{
+            //    Console.WriteLine("Sadrzi");
+            //}
             //Deck deck = new Deck();
             //char input = Console.ReadKey().KeyChar;
             //while (input != 'e')
@@ -76,13 +92,21 @@ namespace Sevens
 
             //    input = Console.ReadKey().KeyChar;
             //}
+
+
             string[] a = { "7", "8", "9", "10", "A", "J", "Q", "K" };
             Random r = new Random();
             for (int i = 0; i < 32; i++)
             {
-                deck.addCard(new Card(a[r.Next(0, 7)],r.Next(3,7)));
-
+                deck.addCard(new Card(a[r.Next(0, a.Length)], r.Next(3, 7)));
+                if (deck.isContains(card))
+                {
+                    Console.WriteLine("Duplikat");
+                }
             }
+
+
+
             //deck.create4Cards("7");
             //deck.create4Cards("8");
             //deck.create4Cards("9");
@@ -93,11 +117,13 @@ namespace Sevens
             //deck.create4Cards("K");
             //deck.REMOVEat(0);
             //Console.Write("{0} ",deck.REMOVEat(0));
-            deck.print();
+
 
             Console.WriteLine();
             Console.WriteLine("Ja sam Mećava i volim da mećavam");
+            
             Console.ReadLine();
+
         }
 
 
